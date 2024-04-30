c881dcf3e8f916e7d2dfa11365fdb3a4696a7229
phunn <phuteobs0166@gmail.com>
2024-04-30 14:27:52 +0700

    wip

--- src/ui-pages/common/layout/admin/left/admin-left-common.js
+++ src/ui-pages/common/layout/admin/left/admin-left-common.js
@@ -8,57 +8,66 @@ import {
     ProductOutlined,
     UsergroupAddOutlined
 } from '@ant-design/icons';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 const { Content, Footer } = Layout;
 const { Search } = Input;
+const classNames = require('classnames');
 
+const menus = [
 
+    {
+        icon: <ProductOutlined style={{ fontSize: '20px' }} />,
+        name: "Dashboard",
+        route: "/admin",
+        isActive: true,
+    },
+    {
+        icon: <UserOutlined style={{ fontSize: '20px' }} />,
+        name: "Identity",
+        route: "/admin/identity",
+        isActive: false,
+    },
+    {
+        icon: <FolderOpenOutlined style={{ fontSize: '20px' }} />,
+        name: "Documents",
+        route: "/admin/documents",
+        isActive: false,
+    },
+    {
+        icon: <BranchesOutlined style={{ fontSize: '20px' }} />,
+        name: "Workflows",
+        route: "/admin/workflows",
+        isActive: false,
+    }
+]
 export const AdminLeftCommomLayout = ({ children }) => {
+    const [listMenu, setListMenu] = useState([]);
 
-    const [menu1,setMenu1] = useState(null)
-    const [menu2,setMenu2] = useState(null)
-    const [menu3,setMenu3] = useState(null)
-    const [menu4,setMenu4] = useState("menu_item_childen_click")
+    useEffect(()=>{
+        setListMenu([...menus])
+    },[])
     const navigate = useNavigate();
-    const onClickMenu = (route, key) => {
+    const onClickMenu = (route, name) => {
+        var newMenus = listMenu.map(x => {
+            if (x.name === name) {
+                return {
+                    ...x,
+                    isActive: true,
+                }
+            } else {
+                return {
+                    ...x,
+                    isActive: false
+                }
+            }
+        });
+        
+        setListMenu([...newMenus]);
         navigate(route);
-        onMenuItemClick(key);
+        // onMenuItemClick(key);
     }
-    const onMenuItemClick = (key) =>{
-        console.log("key",key)
-       if(key === "1"){
-         setMenu1("menu_item_childen_click"); 
-         setMenu2(null);  
-         setMenu3(null);
-         setMenu4(null);
-       }
-       if(key === "1"){
-        setMenu1("menu_item_childen_click"); 
-        setMenu2(null);  
-        setMenu3(null);
-        setMenu4(null);
-      }
-       if(key === "2"){
-        setMenu1(null); 
-        setMenu2("menu_item_childen_click");  
-        setMenu3(null);
-        setMenu4(null);
-      }
-      if(key === "3"){
-        setMenu1(null); 
-        setMenu2(null);  
-        setMenu3("menu_item_childen_click");
-        setMenu4(null);
-      }
-      if(key === "4"){
-        setMenu1(null); 
-        setMenu2(null);  
-        setMenu3(null);
-        setMenu4("menu_item_childen_click");
-      }
-      
-    }
+
     return (
         <div
             style={{
@@ -70,7 +79,7 @@ export const AdminLeftCommomLayout = ({ children }) => {
                 boxShadow: '5px 0px 10px rgba(0, 0, 0, 0.1)'/* Đổ bóng bên phải */
             }}
         >
-            <div style={{ margin:'15px 0' }}>
+            <div style={{ margin: '15px 0' }}>
 
                 <Link to="/" className="menu_left">
                     <div className="logo_avatar menu_left">
@@ -79,32 +88,39 @@ export const AdminLeftCommomLayout = ({ children }) => {
                 </Link>
                 <div className="divider"></div>
             </div>
-            <div className="menu_item" onClick={() => { onClickMenu("/admin/","4") }}>
-                <div className={`menu_item_childen ${menu4}`} >
-                    <ProductOutlined  style={{ fontSize: '20px' }} />
-                    <span>Dashboard</span>
-                </div> 
-            </div>
-            
-            <div className="menu_item" onClick={() => { onClickMenu("/admin/identity","1") }}>
+            {listMenu.length && listMenu.map(x => {
+                return (
+                    <div key={x.name} className="menu_item" >
+                        <div className={classNames('menu_item_childen', `${x.isActive ? "menu_item_childen_click" : ""}`)} onClick={() => { onClickMenu(x.route, x.name) }}>
+                            {x.icon}
+                            <span>{x.name}</span>
+                        </div>
+                    </div>
+                )
+
+            })}
+
+
+
+            {/* <div className="menu_item" onClick={() => { onClickMenu("/admin/identity", "1") }}>
                 <div className={`menu_item_childen ${menu1}`} >
-                    <UserOutlined style={{ fontSize: '20px' }} />
+
                     <span>Identity</span>
-                </div> 
+                </div>
             </div>
-            {/* menu_item_childen */}
-            <div className="menu_item" onClick={() => { onClickMenu("/admin/documents","2") }}>
+        
+            <div className="menu_item" onClick={() => { onClickMenu("/admin/documents", "2") }}>
                 <div className={`menu_item_childen ${menu2}`}>
-                    <FolderOpenOutlined style={{ fontSize: '20px'}} />
+
                     <span>Documents</span>
                 </div>
             </div>
-            <div className="menu_item" onClick={() => { onClickMenu("/admin/workflows","3") }} >
+            <div className="menu_item" onClick={() => { onClickMenu("/admin/workflows", "3") }} >
                 <div className={`menu_item_childen ${menu3}`} >
-                    <BranchesOutlined style={{ fontSize: '20px' }} />
+
                     <span>Workflows</span>
                 </div>
-            </div>
+            </div> */}
             <div className="user_avatar">
                 <Typography.Text style={{ color: '#1890ff' }}>P</Typography.Text>
             </div>
